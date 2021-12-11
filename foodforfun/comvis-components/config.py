1c1ddd03adfbad5dd2a08a33f517011c8da3b8b2
hongeinh <honganh3179@gmail.com>
2021-12-11 11:57:18 +0700

    backend template

---
+++ foodforfun/comvis-components/config.py
@@ -0,0 +1,17 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    CORE_DATA_DIR = '{}/{}'.format(basedir, 'core_data')
+    HOST = 'localhost'
+    PORT = 8080
+    CLIENT_MAX_SIZE = 40 * 1024 ** 2
+
+
+class FoodForFunConfig:
+    CORE_DATA_DIR = '{}/{}/{}'.format(basedir,
+                                      'foodforfun', 'core_data')
+    MODEL_VERSION = 1
+
+
