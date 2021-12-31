6bca146e33ae6aa625c8e1bdda2a6e472039c7df
hongeinh <honganh3179@gmail.com>
2021-12-31 15:21:39 +0700

    add model service

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,17 @@
 
 echo "This project is credited by HoHuHuPhuHo. Let's start!"
 
+if [[ $(docker ps -a | grep foodforfun) ]]; then
+    echo 'Food for fun exists'
+    docker rm -f foodforfun
+else 
+    echo 'No food for fun'
+fi
+
 docker build -t foodforfun:1.0 .
 
-docker run -n foodforfun \
--p 8080:8080 \
+docker run \
+--name foodforfun \
+-p 4321:4321 \
 --restart always \
 foodforfun:1.0
