7e272b81efe46b1ccb9870e43478df0319616c18
hongeinh <honganh3179@gmail.com>
2022-01-12 17:54:19 +0700

    round accuracy

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,17 @@
 
 echo "This project is credited by HoHuHuPhuHo. Let's start!"
 
-if [[ $(docker ps -a | grep foodforfun-1.2) ]]; then
+if [[ $(docker ps -a | grep foodforfun-1.4) ]]; then
     echo 'Food for fun exists'
-    docker rm -f foodforfun-1.2
+    docker rm -f foodforfun-1.4
 else 
     echo 'No food for fun'
 fi
 
-docker build -t foodforfun:1.2 .
+docker build -t foodforfun:1.4 .
 
 docker run \
---name foodforfun-1.2 \
+--name foodforfun-1.4 \
 -p 4321:4321 \
 --restart always \
-foodforfun:1.2
+foodforfun:1.4
