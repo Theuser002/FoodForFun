7ab251bc51d099ccf99491d9eadb9e093427b516
Hồng Einh <44611968+hongeinh@users.noreply.github.com>
2022-01-13 07:54:30 +0700

    Feature/mobilenet (#7)
    * round accuracy
    
    * round accuracy
    
    * add mobilenet class
    
    * modify readme

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-echo "This project is credited by HoHuHuPhuHo. Let's start!"
+echo "Vietnamese Food Recognition project is credited by HoHuHuPhuHo. Let's start!"
 
-if [[ $(docker ps -a | grep foodforfun-1.2) ]]; then
+if [[ $(docker ps -a | grep foodforfun-$1) ]]; then
     echo 'Food for fun exists'
-    docker rm -f foodforfun-1.2
+    docker rm -f foodforfun-$1
 else 
     echo 'No food for fun'
 fi
 
-docker build -t foodforfun:1.2 .
+docker build -t foodforfun:$1 .
 
 docker run \
---name foodforfun-1.2 \
--p 4321:4321 \
+--name foodforfun-$1 \
+-p $2:4321 \
 --restart always \
-foodforfun:1.2
+foodforfun:$1
